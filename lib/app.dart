6c9207c10b2130badcebce3e81ccd71ea309765f
Brij Pratap Singh <brijjadaun@gmail.com>
2020-05-15 00:20:52 +0530

    profile image added

--- lib/app.dart
+++ lib/app.dart
@@ -15,6 +15,50 @@ class App extends StatelessWidget
     appBar: AppBar(
       title: Text("Profile UI"),
     ),
+    body:
+    Container(
+      decoration: BoxDecoration(
+        gradient:LinearGradient(
+          begin:Alignment.topLeft,
+          end:Alignment.bottomRight,
+          colors:[
+            Colors.green[300],
+            Colors.green[50]
+          ]
+        )
+      ),
+      child: Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 80,
+            backgroundImage: AssetImage("images/profile.jpg"),
+          ),
+          //Image.asset("images/profile.jpg",width: 200,height: 100,),
+          ListTile(
+            title: Center(child:Text("Brij Patap Singh",style: TextStyle(fontSize:20),)),
+            subtitle: Center(child:Text("Mobile Application Developer",style: TextStyle(fontSize:15),)),
+          ),
+          Row(
+            mainAxisAlignment:MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              RaisedButton(onPressed:()
+          {},child: Text("Message"),
+          ),
+          RaisedButton(onPressed: ()
+          {
+            
+          },child: Text("Hire Me"),
+          ),
+            ],
+          ),
+          ListTile(
+            title:Text("About Me",style: TextStyle(fontSize:20),),
+            subtitle: Text("2.5 year of experience in Android and i have develope no. of project to develope my skill through practicing code ",style: TextStyle(fontSize:15),),
+          ),
+        ],
+      ),
+    ),
+    
   ),
     );
   }
